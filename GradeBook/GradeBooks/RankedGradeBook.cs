c03bf352064bae0bcb613514478320fdcfefad65
Krupa144 <kacpi8450@gmail.com>
2024-03-12 14:52:13 +0100

    5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -53,14 +53,16 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStatistics()
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
             }
-            else if(Students.Count >= 5)
+            else
             {
-                Console.WriteLine();
+                base.CalculateStatistics();
             }
+            
+            
         }
     }
 }
