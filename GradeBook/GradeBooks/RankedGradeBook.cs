ea9a6d5822c167a37d559716be327604a207c2d3
Krupa144 <kacpi8450@gmail.com>
2024-03-12 12:40:47 +0100

    4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,16 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) :base (name) 
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Assessment requires 5 students");
+            }
+
+            int rank = 1;
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                    rank++;
+            }
+
+            var percent = (double)rank / Students.Count;
+
+            if (percent >= 0.8)
+            {
+                return 'A';
+            }
+            else if (percent >= 0.6)
+            {
+                return 'B';
+            }
+            else if (percent >= 0.4)
+            {
+                return 'C';
+            }
+            else if (percent >= 0.2)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
+
