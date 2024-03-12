022e96965541f4696da182c973c2107f66ffc69b
Krupa144 <kacpi8450@gmail.com>
2024-03-12 11:37:19 +0100

    2

--- GradeBook/Student.cs
+++ GradeBook/Student.cs
@@ -12,6 +12,8 @@ namespace GradeBook
         public string Name { get; set; }
         public StudentType Type { get; set; }
         public EnrollmentType Enrollment { get; set; }
+
+        public Type GradebookType { get; set; }
         public List<double> Grades { get; set; }
         [JsonIgnore]
         public double AverageGrade
